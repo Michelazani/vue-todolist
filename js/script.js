ca34b282a1302696c6f1d8d073123f940f0277e5
Michelazani <michi.zani220@gmail.com>
2023-12-04 16:00:39 +0100

    creato ciclo for per inserimento dati HTML

--- js/script.js
+++ js/script.js
@@ -3,7 +3,31 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'hello vue'
+            namesListPupils : [
+                {text: 'Alessia', done: 'false'},
+                {text: 'Beatrice', done: 'true'},
+                {text: 'Carla', done: 'false'},
+                {text: 'Davide', done: 'true'},
+                {text: 'Elia', done: 'false'},
+                {text: 'Flavio', done: 'true'},
+                {text: 'Giorgio', done: 'false'},
+                {text: 'Ilaria', done: 'true'},
+                {text: 'Laura', done: 'false'},
+                {text: 'Mario', done: 'true'},
+                {text: 'Niccolò', done: 'false'},
+                {text: 'Orazio', done: 'true'},
+                {text: 'Paola', done: 'false'},
+                {text: 'Quasimodo', done: 'true'},
+                {text: 'Roberta', done: 'false'},
+                {text: 'Sara', done: 'true'},
+                {text: 'Ugo', done: 'false'},
+                {text: 'Valeria', done: 'true'}
+            ],
         }
     },
+
+    methods : {
+        
+    }
+    
 }).mount('#app'); 
