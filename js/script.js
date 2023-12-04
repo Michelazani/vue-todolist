7184aceaecf685d3b6108b54c84413fedb444fbe
Michelazani <michi.zani220@gmail.com>
2023-12-04 17:12:19 +0100

    v-if e v-else

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-const { createApp } = Vue;
+const {createApp} =Vue;
 
 createApp({
     data() {
         return {
             namesListPupils : [
-                {text: 'Alessia', done: 'false'},
-                {text: 'Beatrice', done: 'true'},
-                {text: 'Carla', done: 'false'},
-                {text: 'Davide', done: 'true'},
-                {text: 'Elia', done: 'false'},
-                {text: 'Flavio', done: 'true'},
-                {text: 'Giorgio', done: 'false'},
-                {text: 'Ilaria', done: 'true'},
-                {text: 'Laura', done: 'false'},
-                {text: 'Mario', done: 'true'},
-                {text: 'Niccolò', done: 'false'},
-                {text: 'Orazio', done: 'true'},
-                {text: 'Paola', done: 'false'},
-                {text: 'Quasimodo', done: 'true'},
-                {text: 'Roberta', done: 'false'},
-                {text: 'Sara', done: 'true'},
-                {text: 'Ugo', done: 'false'},
-                {text: 'Valeria', done: 'true'}
+                {text: 'Alessia', done: false},
+                {text: 'Beatrice', done: true},
+                {text: 'Carla', done: false},
+                {text: 'Davide', done: true},
+                {text: 'Elia', done: false},
+                {text: 'Flavio', done: true},
+                {text: 'Giorgio', done: false},
+                {text: 'Ilaria', done: true},
+                {text: 'Laura', done: false},
+                {text: 'Mario', done: true},
+                {text: 'Niccolò', done: false},
+                {text: 'Orazio', done: true},
+                {text: 'Paola', done: false},
+                {text: 'Quasimodo', done: true},
+                {text: 'Roberta', done: false},
+                {text: 'Sara', done: true},
+                {text: 'Ugo', done: false},
+                {text: 'Valeria', done: true}
             ],
         }
     },
